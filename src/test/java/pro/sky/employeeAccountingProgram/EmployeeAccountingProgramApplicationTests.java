1fb99318c9ac7cb288378d96ae164b5986738b09
linda <charming.linda85@gmail.com>
2024-05-16 13:38:01 +0300

    Initial commit

---
+++ src/test/java/pro/sky/employeeAccountingProgram/EmployeeAccountingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeAccountingProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAccountingProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
